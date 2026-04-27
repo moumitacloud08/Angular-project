a229f6d4d59cbbcd360e685b5dc6e9745d26d3e8
Moumita Ray <moumitacloud08@gmail.com>
2026-04-27 11:42:33 +0530

    localstorage and ngOninit

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
+  
   newAppointmentTitle: string = '';
   newAppointmentDate: Date = new Date();
 
   appointments: Appointment[] = [];
+
+  ngOnInit(): void {
+   let saveAppointments = localStorage.getItem("appointments");
+   this.appointments = saveAppointments ? JSON.parse(saveAppointments):[]
+  }
 
   addAppointment() {
     if (this.newAppointmentTitle.trim().length > 0 && this.newAppointmentDate) {
@@ -23,11 +30,16 @@ export class AppointmentListComponent {
 
       this.newAppointmentTitle="";
       this.newAppointmentDate = new Date();
+
+      localStorage.setItem("appointments",JSON.stringify(this.appointments));
       
     }
   }
 
   deleteAppointment(index: number){
     this.appointments.splice(index,1)
+     localStorage.setItem("appointments",JSON.stringify(this.appointments));
   }
+
+
 }
