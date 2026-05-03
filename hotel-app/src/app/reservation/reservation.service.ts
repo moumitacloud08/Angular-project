34e33b344acf6d845c3944b0178a77fe39812696
Moumita Ray <moumitacloud08@gmail.com>
2026-05-03 10:41:38 +0530

    create reservation service

--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
+  private reservations: Reservation[] = [];
 
-  constructor() { }
+  getRerservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getRerservation(id: string): Reservation | undefined {
+    return this.reservations.find((res) => res.id === id);
+  }
+
+  addReservation(revervation: Reservation): void {
+    this.reservations.push(revervation);
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex((res) => res.id == id);
+    this.reservations.splice(index, 1);
+  }
+
+  updateReservation(updateReservation: Reservation) {
+    let index = this.reservations.findIndex(
+      (res) => res.id == updateReservation.id,
+    );
+    this.reservations[index] = updateReservation;
+  }
 }
